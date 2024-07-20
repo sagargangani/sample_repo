42114fed0f4b4cc8359a5326e50a4e8fdf49f42f
Sagar Gangani <59004555+sagargangani@users.noreply.github.com>
2024-07-20 14:14:10 -0400

    updated index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.set("views", path.join(__dirname, "views"));
 // Database configuration
 const config = {
   user: "azureuser",
-  password: "P@ssw0rdssss",
+  password: "P@ssw0rd",
   server: "week10pt1.database.windows.net",
   database: "week10",
   options: {
